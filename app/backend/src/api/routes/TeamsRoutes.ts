91e6dea65a8758ec1d85b146b34f97579904cb27
caren <carenoliveirasilvapontes@gmail.com>
2023-02-28 17:47:03 -0300

    fix:rotas de teams

--- app/backend/src/api/routes/TeamsRoutes.ts
+++ app/backend/src/api/routes/TeamsRoutes.ts
@@ -1,11 +1,11 @@
 import { Router, Request, Response } from 'express';
-import TeamService from '../services/index';
-import TeamController from '../controllers/index';
+import { TeamService } from '../services/index';
+import { TeamController } from '../controllers/index';
 
-const teamsRouter = Router();
+const teamRouter = Router();
 const teamsService = new TeamService();
 const teamController = new TeamController(teamsService);
-teamsRouter.get('/', (req:Request, res:Response) => teamController.readAll(req, res));
-teamsRouter.get('/:id', (req:Request, res:Response) => teamController.readOne(req, res));
+teamRouter.get('/', (req:Request, res:Response) => teamController.readAll(req, res));
+teamRouter.get('/:id', (req:Request, res:Response) => teamController.readOne(req, res));
 
-export default teamsRouter;
+export default teamRouter;
