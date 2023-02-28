0ed1906caf9cf0d0ba199eb2f8487eed04a3359a
caren <carenoliveirasilvapontes@gmail.com>
2023-02-28 17:47:28 -0300

    fix:index controller

---
+++ app/backend/src/api/middleware/validateToken.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import { decodeToken } from '../../utils/token';
+
+const validateToken = (req:Request, res:Response, next:NextFunction) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: 'Token not found' });
+  try {
+    const decoded = decodeToken(authorization);
+    res.locals.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Token must be a valid token' });
+  }
+};
+export default validateToken;
