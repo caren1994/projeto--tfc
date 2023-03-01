fd322fa32e5ccecbd54b8405fbc004bb802de3e2
caren <carenoliveirasilvapontes@gmail.com>
2023-03-01 17:42:34 -0300

    feat:implementação controller finish

--- app/backend/src/api/controllers/MatcheController.ts
+++ app/backend/src/api/controllers/MatcheController.ts
@@ -17,5 +17,11 @@ class MatcheController {
     const result = await this._service.readAll(inProgress === 'true');// faço a comparação da strings que vai me retornar um true ou false
     res.status(200).json(result);
   }
+
+  public async finish(req:Request, res:Response):Promise<Response | void > {
+    const { id } = req.params;
+    const result = await this._service.finish(+id);
+    return res.status(200).json(result);
+  }
 }
 export default MatcheController;
