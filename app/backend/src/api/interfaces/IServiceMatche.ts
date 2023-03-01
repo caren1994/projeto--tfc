207e547b20d617dd3cab0393ad6a62a33ad5de15
caren <carenoliveirasilvapontes@gmail.com>
2023-03-01 17:42:02 -0300

    feat:interface service implementada

--- app/backend/src/api/interfaces/IServiceMatche.ts
+++ app/backend/src/api/interfaces/IServiceMatche.ts
@@ -2,4 +2,5 @@ import Matche from '../../database/models/Matche';
 
 export default interface IServiceMatche{
   readAll(inProgress?:boolean):Promise<Matche[] >;
+  finish(id:number):Promise<number[] | undefined>;
 }
