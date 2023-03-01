e49d93f67d7d878d20e0930d7c7a0b0c9d08f187
caren <carenoliveirasilvapontes@gmail.com>
2023-03-01 17:42:21 -0300

    feat:routes matche finish

--- app/backend/src/api/routes/MatcheRoutes.ts
+++ app/backend/src/api/routes/MatcheRoutes.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express';
 import { MatcheController } from '../controllers/index';
+import validateToken from '../middleware/validateToken';
 import { MatcheService } from '../services/index';
 
 const matcheRouter = Router();
@@ -7,5 +8,10 @@ const matcheService = new MatcheService();
 const matcheController = new MatcheController(matcheService);
 
 matcheRouter.get('/', (req:Request, res:Response) => matcheController.readAll(req, res));
+matcheRouter.patch(
+  '/:id/finish',
+  validateToken,
+  (req:Request, res:Response) => matcheController.finish(req, res),
+);
 
 export default matcheRouter;
