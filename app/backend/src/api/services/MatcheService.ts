9a63c3dbc72061162b90856b44387f726b8839de
caren <carenoliveirasilvapontes@gmail.com>
2023-03-01 16:50:27 -0300

    feat:implementação controller matche agradecimento a thiago lopes

--- app/backend/src/api/services/MatcheService.ts
+++ app/backend/src/api/services/MatcheService.ts
@@ -1,17 +1,28 @@
-import { ModelStatic } from 'sequelize';
+import { ModelStatic, Op } from 'sequelize';
 import Matche from '../../database/models/Matche';
 import IServiceMatche from '../interfaces/IServiceMatche';
 import Team from '../../database/models/Team';
 
 class MatcheService implements IServiceMatche {
   protected model:ModelStatic <Matche> = Matche;
-  async readAll():Promise<Matche[]> {
-    const result = await this.model.findAll({
-      include: [
+
+  async readAll(inProgress?: boolean): Promise<Matche[]> {
+    if (inProgress === undefined) {
+      const result = await this.model.findAll({
+        include: [
+          { model: Team, as: 'homeTeam', attributes: ['teamName'] },
+          { model: Team, as: 'awayTeam', attributes: ['teamName'] },
+        ],
+      });
+      return result;
+    }
+    const result = await this.model.findAll(
+      { include: [
         { model: Team, as: 'homeTeam', attributes: ['teamName'] },
         { model: Team, as: 'awayTeam', attributes: ['teamName'] },
       ],
-    });
+      where: { [Op.and]: [{ inProgress }] } }, // Sequelize inferiu que o chamador queria um ANDpara as duas verificações. O código acima é equivalente a:
+    );
     return result;
   }
 }
