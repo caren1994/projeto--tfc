df33e5d5b0d499efe2e3e2d49308c90b0e51345c
caren <carenoliveirasilvapontes@gmail.com>
2023-03-01 19:33:50 -0300

    feat:interface do service matche creatematche implementado

--- app/backend/src/api/interfaces/IServiceMatche.ts
+++ app/backend/src/api/interfaces/IServiceMatche.ts
@@ -1,8 +1,10 @@
 import Matche from '../../database/models/Matche';
+import IBodyCreateMatche from './IBodyCreateMatche';
 import IBodyMatche from './IBodyMatche';
 
 export default interface IServiceMatche{
   readAll(inProgress?:boolean):Promise<Matche[] >;
   finish(id:number):Promise<number[] | undefined>;
   updateMatche(id:number, body:IBodyMatche):Promise<number[] | undefined>;
+  createMatche(body:IBodyCreateMatche):Promise<Matche>;
 }
