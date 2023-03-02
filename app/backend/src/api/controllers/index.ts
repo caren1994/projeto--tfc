3b278d360df1335db1b83e9556dc57a7f67de723
caren <carenoliveirasilvapontes@gmail.com>
2023-03-02 15:29:25 -0300

    feat:index controller leader

--- app/backend/src/api/controllers/index.ts
+++ app/backend/src/api/controllers/index.ts
@@ -1,9 +1,11 @@
 import TeamController from './TeamController';
 import UserController from './UserController';
 import MatcheController from './MatcheController';
+import LeaderBoardController from './leaderBoardController';
 
 export {
   TeamController,
   UserController,
   MatcheController,
+  LeaderBoardController,
 };
